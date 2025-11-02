6819eabfaf1158366983848cfd5568cf559571f2
Michelle Baird <mibaird@365.bellevue.edu>
2025-11-02 08:01:25 -0600

    Completed week 2 assignments

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,39 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Michelle Baird
+  Date: October 30, 2025
+  Filename: script.js
 */
 
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+    getName: function() {
+      return name;
+    },
+    getGender: function() {
+      return gender;
+    },
+    getClass: function() {
+      return characterClass;
+    }
+  };
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  const name = document.getElementById("heroName").value;
+  const gender = document.getElementById("heroGender").value;
+  const characterClass = document.getElementById("heroClass").value;
 
-  // TODO: Create character
+  const hero = createCharacter (name, gender, characterClass)
 
-  // TODO: Display character information
+document.getElementById("characterOutput").innerHTML =
+    "<p><strong>Name:</strong> " + hero.getName() + "</p>" +
+    "<p><strong>Gender:</strong> " + hero.getGender() + "</p>" +
+    "<p><strong>Class:</strong> " + hero.getClass() + "</p>";
+
 });
